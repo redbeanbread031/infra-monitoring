642b4e27431c9594a4b14771499b972f6a04892d
redbeanbread031 <redbeanbread031@gmail.com>
2026-02-20 02:06:18 +0900

    refactor: extract AbnormalMetricLog mapping logic(from ThresholdService) into threshold mapper package

--- api-backend/src/main/java/kr/cs/interdata/api_backend/service/ThresholdService.java
+++ api-backend/src/main/java/kr/cs/interdata/api_backend/service/ThresholdService.java
@@ -26,6 +26,7 @@ import kr.cs.interdata.api_backend.service.repository_service.ContainerInventory
 import kr.cs.interdata.api_backend.service.repository_service.MonitoringDefinitionService;
 import kr.cs.interdata.api_backend.service.threshold.ThresholdEventService;
 import kr.cs.interdata.api_backend.service.threshold.ThresholdPolicyService;
+import kr.cs.interdata.api_backend.service.threshold.mapper.AbnormalMetricLogMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,15 +43,15 @@ public class ThresholdService {
     private final Logger logger = LoggerFactory.getLogger(ThresholdService.class);
 
     private final AbnormalDetectionService abnormalDetectionService;
-    private final ContainerInventoryService containerInventoryService;
+    private final AbnormalMetricLogMapper abnormalMetricLogMapper;
     private final AbnormalMetricLogRepository abnormalMetricLogRepository;
 
     @Autowired
     public ThresholdService(AbnormalDetectionService abnormalDetectionService,
-                            ContainerInventoryService containerInventoryService,
+                            AbnormalMetricLogMapper abnormalMetricLogMapper,
                             AbnormalMetricLogRepository abnormalMetricLogRepository) {
         this.abnormalDetectionService = abnormalDetectionService;
-        this.containerInventoryService = containerInventoryService;
+        this.abnormalMetricLogMapper = abnormalMetricLogMapper;
         this.abnormalMetricLogRepository = abnormalMetricLogRepository;
     }
 
@@ -64,7 +65,7 @@ public class ThresholdService {
         List<AbnormalMetricLog> logs = abnormalDetectionService.getLatestAbnormalMetricsByMachineId(machineId.getTargetId());
 
         // 결과를 클라이언트에 맞게 매핑 및 반환
-        return getMapList(logs);
+        return abnormalMetricLogMapper.getMapList(logs);
     }
 
     /**
@@ -183,46 +184,8 @@ public class ThresholdService {
         List<AbnormalMetricLog> logs = abnormalDetectionService.getLatestAbnormalMetrics();
 
         // 결과를 클라이언트에 맞게 매핑 및 반환
-        return getMapList(logs);
+        return abnormalMetricLogMapper.getMapList(logs);
     }
 
-    /**
-     * AbnormalMetricLog 리스트를 클라이언트에 반환할 Map 리스트 형식으로 변환한다.
-     * <p>
-     * 각 로그의 정보를 Map으로 변환하며,
-     * 만약 machineType이 "container"인 경우 containerInventoryService를 통해 해당 컨테이너의 hostName을 추가한다.
-     * 기타 정보(timestamp, messageType, machineType, machineId, machineName, metricName, threshold, value)도 포함된다.
-     * </p>
-     *
-     * @param logs AbnormalMetricLog 객체 리스트
-     * @return 각 로그를 Map<String, Object>로 변환한 리스트
-     */
-    private List<Map<String, Object>> getMapList(List<AbnormalMetricLog> logs) {
-        List<Map<String, Object>> result = new ArrayList<>();
-        for (AbnormalMetricLog log : logs) {
-            Map<String, Object> record = new HashMap<>();
-
-            // ContainerInventory 엔티티의 machineId와 machineName을 파싱해서 둘 조합이 있으면 종속된 hostName을 넘겨줌
-            String hostName;
-            if (log.getMachineType().equals("container")) {
-                hostName = containerInventoryService.getHostNameByContainerId(log.getMachineId());
-            } else {
-                hostName = log.getMachineName();
-            }
-
-            record.put("timestamp", log.getTimestamp().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
-            record.put("messageType", log.getMessageType());
-            record.put("machineType", log.getMachineType());
-            record.put("machineId", log.getMachineId());
-            record.put("machineName", log.getMachineName());
-            record.put("metricName", log.getMetricName());
-            record.put("hostName", hostName);
-            record.put("threshold", log.getThreshold());
-            record.put("value", log.getValue());
-
-            result.add(record);
-        }
-        return result;
-    }
 
 }
